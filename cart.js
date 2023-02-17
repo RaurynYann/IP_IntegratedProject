e350d05bce3978d1b3f4f8e44c6b8c97b5e5bd88
ytinggzz <s10243643@connect.np.edu.sg>
2023-02-18 01:22:55 +0800

    Added more java in

--- cart.js
+++ cart.js
@@ -1,37 +1,84 @@
-// Get the cart items from localStorage
 let cartItems = JSON.parse(localStorage.getItem('cartItems'));
-
-// Display the cart items
 let cartItemsContainer = document.querySelector('.cart-items');
 let orderSubtotalElement = document.querySelector('#order-subtotal');
 let orderTotalElement = document.querySelector('#order-total');
 
 if (cartItems && cartItems.length > 0) {
   let orderSubtotal = 0;
+  let cartItemElements = {};
+
+  // Loop through the cart items and display them in the cart view
   for (let i = 0; i < cartItems.length; i++) {
     let product = cartItems[i];
-    let cartItem = document.createElement('div');
-    cartItem.classList.add('box1');
-    cartItem.innerHTML = `
-      <img src="${product.image}">
-      <div class="bagcontent">
-        <h3>${product.name}</h3>
-        <h4>Price: $${product.price.toFixed(2)}</h4>
-        <p class="btn-area"><i class="fa fa-trash" data-index="${i}"></i></p>
-        <div class="quantity">
-          <div class="btn plus">+</div>
-          <div class="count">1</div>
-          <div class="btn minus">-</div>
+
+    // Create a unique key for the cart item based on the product name
+    let cartItemKey = product.name;
+
+    // If there is already a cart item for this product, update its quantity
+    if (cartItemKey in cartItemElements) {
+      let cartItemElement = cartItemElements[cartItemKey];
+      let countElement = cartItemElement.querySelector('.count');
+      let count = parseInt(countElement.innerText);
+      count += 1;
+      countElement.innerText = count;
+
+      let priceElement = cartItemElement.querySelector('.bagcontent h4');
+      let productPrice = parseFloat(product.price);
+
+      let newPrice = productPrice * count;
+      priceElement.innerText = `Price: $${newPrice.toFixed(2)}`;
+
+    } else {
+      // If there is no cart item for this product yet, create a new one
+      let cartItem = document.createElement('div');
+      cartItem.classList.add('box1');
+      cartItem.innerHTML = `
+        <img src="${product.image}">
+        <div class="bagcontent">
+          <h3>${product.name}</h3>
+          <h4>Price: $${parseFloat(product.price).toFixed(2)}</h4>
+          <p class="btn-area"><i class="fa fa-trash" data-index="${i}"></i></p>
+          <div class="quantity">
+            <div class="btn plus">+</div>
+            <div class="count">1</div>
+            <div class="btn minus">-</div>
+          </div>
         </div>
-      </div>
-    `;
-    cartItemsContainer.appendChild(cartItem);
-    orderSubtotal += product.price;
+      `;
+      cartItemsContainer.appendChild(cartItem);
+
+      // Store a reference to the cart item element for this product
+      cartItemElements[cartItemKey] = cartItem;
+
+      // Add event listener to quantity input field
+      let countElement = cartItem.querySelector('.count');
+      countElement.addEventListener('input', function() {
+        let count = parseInt(countElement.innerText);
+        if (isNaN(count)) {
+          count = 1;
+        }
+        product.quantity = count;
+        localStorage.setItem('cartItems', JSON.stringify(cartItems));
+      });
+
+      // Update quantity in local storage
+      product.quantity = 1;
+      localStorage.setItem('cartItems', JSON.stringify(cartItems));
+    }
+
+    // Calculate the order subtotal by multiplying the product price with its count in the cart
+    let productPrice = parseFloat(product.price);
+    if (isNaN(productPrice)) {
+      console.error(`Invalid price for product: ${product.name}`);
+      continue;
+    }
+    orderSubtotal += productPrice * product.quantity;
   }
+
+  // Display the order subtotal and total
   orderSubtotalElement.innerText = `$${orderSubtotal.toFixed(2)}`;
   orderTotalElement.innerText = `$${orderSubtotal.toFixed(2)}`;
-
-  // Add event listener to the trash icon
+  // Add functionality to the trash icons, plus buttons, and minus buttons
   let trashIcons = document.querySelectorAll('.fa-trash');
   for (let i = 0; i < trashIcons.length; i++) {
     let trashIcon = trashIcons[i];
@@ -40,11 +87,10 @@ if (cartItems && cartItems.length > 0) {
       cartItems.splice(index, 1);
       localStorage.setItem('cartItems', JSON.stringify(cartItems));
       alert('The product was removed from your shopping bag.');
-      location.reload(); // Refresh the page to update the cart
+      location.reload(); 
     });
   }
 
-  // Add event listeners to the plus and minus buttons
   let plusButtons = document.querySelectorAll('.btn.plus');
   let minusButtons = document.querySelectorAll('.btn.minus');
   for (let i = 0; i < plusButtons.length; i++) {
@@ -53,29 +99,30 @@ if (cartItems && cartItems.length > 0) {
     let countElement = plusButton.parentNode.querySelector('.count');
     let count = parseInt(countElement.innerText);
 
-    // Add event listener for plus button
     plusButton.addEventListener('click', function() {
       count++;
       countElement.innerText = count;
       alert('Product quantity updated.');
-      orderSubtotal += parseFloat(plusButton.parentNode.parentNode.querySelector('h4').textContent.split('$')[1]);
-      orderSubtotalElement.innerText = `$${orderSubtotal.toFixed(2)}`;
-      orderTotalElement.innerText = `$${orderSubtotal.toFixed(2)}`;
-    });
-
-    // Add event listener for minus button
-    minusButton.addEventListener('click', function() {
-      if (count > 1) {
-        count--;
-        countElement.innerText = count;
-        orderSubtotal -= parseFloat(minusButton.parentNode.parentNode.querySelector('h4').textContent.split('$')[1]);
+        let productPrice = parseFloat(plusButton.parentNode.parentNode.querySelector('h4').textContent.split('$')[1]);
+        orderSubtotal += productPrice;
         orderSubtotalElement.innerText = `$${orderSubtotal.toFixed(2)}`;
         orderTotalElement.innerText = `$${orderSubtotal.toFixed(2)}`;
-      }
-    });
-  }
-} else {
-  let emptyCartMessage = document.createElement('p');
-  emptyCartMessage.innerText = 'Your shopping bag is empty';
-  cartItemsContainer.appendChild(emptyCartMessage);
+      });
+  
+      minusButton.addEventListener('click', function() {
+        if (count > 1) {
+          count--;
+          countElement.innerText = count;
+          let productPrice = parseFloat(minusButton.parentNode.parentNode.querySelector('h4').textContent.split('$')[1]);
+          orderSubtotal -= productPrice;
+          orderSubtotalElement.innerText = `$${orderSubtotal.toFixed(2)}`;
+          orderTotalElement.innerText = `$${orderSubtotal.toFixed(2)}`;
+        }
+      });
+  
+    }
+  } else {
+    let emptyCartMessage = document.createElement('p');
+    emptyCartMessage.innerText = 'Your shopping bag is empty';
+    cartItemsContainer.appendChild(emptyCartMessage);
 }
