ba9fa8e63824de4c39f5bd50343fa950ba54264b
ytinggzz <s10243643@connect.np.edu.sg>
2023-02-18 02:20:07 +0800

    Chaneg the javascript for image.js

--- image.js
+++ image.js
@@ -1,15 +1,8 @@
 var bigimg = document.getElementById("main");
 var smallimg = document.getElementsByClassName("small");
 
-smallimg[0].onclick = function(){
-    bigimg.src = smallimg[0].src;
-}
-smallimg[1].onclick = function(){
-    bigimg.src = smallimg[1].src;
-}
-smallimg[2].onclick = function(){
-    bigimg.src = smallimg[2].src;
-}
-smallimg[3].onclick = function(){
-    bigimg.src = smallimg[3].src;
+for (var i = 0; i < smallimg.length; i++) {
+  smallimg[i].addEventListener("click", function() {
+    bigimg.src = this.src;
+  });
 }
